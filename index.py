e3ac12ef1c71a9c6280b6a3df464adcd76ebaa00
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-22 22:16:16 -0500

    Debug error: Unable to import module 'now__handler__python': cannot import name 'etree'

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 
-from kdnfeed.feed import feed
+# from kdnfeed.feed import feed
 
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/xml; charset=UTF-8')
+        # self.send_header('Content-type', 'text/xml; charset=UTF-8')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(feed())
+        self.wfile.write('asdf'.encode())
