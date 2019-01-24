62345a40075af7283c9686af72ee74053b1eff91
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-23 19:43:26 -0500

    Set kdnfeed logger level to INFO

--- kdnfeed/config.py
+++ kdnfeed/config.py
@@ -6,7 +6,7 @@ def configure_logging() -> None:
     path = Path(__file__).with_name('logging.conf')
     logging.config.fileConfig(path)
     log = logging.getLogger(__name__)
-    log.info('Logging is configured.')
+    log.debug('Logging is configured.')
 
 
 INPUT_FEED_URL = 'https://www.kdnuggets.com/feed'
