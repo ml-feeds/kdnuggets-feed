a18bd63f36e27b33155f5866a5f2409040fc0e5a
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-23 22:04:14 -0500

    Sort blacklist

---
+++ scripts/sort_blacklist.py
@@ -0,0 +1,15 @@
+import logging
+
+from kdnfeed import config
+
+config.configure_logging()
+
+log = logging.getLogger(__name__)
+
+df = config.BLACKLIST.copy()
+df = df.sort_values(df.columns.tolist()).drop_duplicates()
+if not df.equals(config.BLACKLIST):
+    df.to_csv(config.BLACKLIST_PATH, index=False)
+    log.info('Sorted the blacklist file.')
+else:
+    log.info('The blacklist file is already sorted.')
