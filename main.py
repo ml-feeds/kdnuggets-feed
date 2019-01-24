038ac816ee3aa586ce1552ad82b46c9bb0e79acc
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-23 19:40:19 -0500

    Set Content-Type text/xml

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from kdnfeed.feed import feed
 
 
 def serve(request: flask.Request) -> bytes:
-    return feed()
+    return feed(), 200, {'Content-Type': 'text/xml; charset=utf-8'}  # Alt: application/rss+xml; charset=utf-8
