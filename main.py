ec24148048dc27e6bf179b7607a7c039dcce8012
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-23 19:54:03 -0500

    Revert Content-Type to text/xml

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from kdnfeed.feed import feed
 
 
 def serve(request: flask.Request) -> Tuple[bytes, int, Dict[str, str]]:
-    return feed(), 200, {'Content-Type': 'application/rss+xml; charset=utf-8'}  # Alt: text/xml; charset=utf-8
+    return feed(), 200, {'Content-Type': 'text/xml; charset=utf-8'}
