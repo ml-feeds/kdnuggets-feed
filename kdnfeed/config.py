d1435b75de53e494b54c9670d18c39a156832718
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-27 12:35:05 -0500

    Avoid logging time if on serverless

--- kdnfeed/config.py
+++ kdnfeed/config.py
@@ -16,7 +16,7 @@ BLACKLIST_PATH = Path(__file__).with_name('blacklist.csv')
 BLACKLIST = pd.read_csv(BLACKLIST_PATH)
 CACHE_TTL = 14 * 60
 INPUT_FEED_URL = 'https://www.kdnuggets.com/feed'
-ON_CLOUD = bool(os.getenv('GCLOUD_PROJECT'))
+ON_SERVERLESS = bool(os.getenv('GCLOUD_PROJECT'))
 OPERATORS = {
     'contains': operator.contains,
     'equals': operator.eq,
@@ -29,20 +29,23 @@ LOGGING = {  # Ref: https://docs.python.org/3/howto/logging.html#configuring-log
     'version': 1,
     'formatters': {
         'detailed': {
-            'format': '%(relativeCreated)i %(thread)x:%(name)s:%(lineno)d:%(funcName)s:%(levelname)s: %(message)s',
+            'format': '[%(relativeCreated)i] %(thread)x:%(name)s:%(lineno)d:%(funcName)s:%(levelname)s: %(message)s',
+        },
+        'serverless': {
+            'format': '%(thread)x:%(name)s:%(lineno)d:%(funcName)s:%(levelname)s: %(message)s',
         },
     },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
             'level': 'DEBUG',
-            'formatter': 'detailed',
+            'formatter': 'serverless' if ON_SERVERLESS else 'detailed',
             'stream': 'ext://sys.stdout',
         },
     },
     'loggers': {
         PACKAGE_NAME: {
-            'level': 'DEBUG' if not ON_CLOUD else 'INFO',
+            'level': 'INFO' if ON_SERVERLESS else 'DEBUG',
             'handlers': ['console'],
             'propagate': False,
         },
