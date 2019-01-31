02dc6f33b46a5cec17dbbf4bb7a3f51b07fd4d06
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-31 12:48:02 -0500

    Fix typo

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ def serve(request: flask.Request) -> Tuple[bytes, int, Dict[str, str]]:
     hget = request.headers.get
     log.info('Received request from %s from %s, %s, %s.', hget('X-Appengine-User-Ip'),
              hget('X-Appengine-City'), hget('X-Appengine-Region'), hget('X-Appengine-Country'))
-    output = feed.fee()
+    output = feed.feed()
     mem.log_use()
     return output, 200, {'Content-Type': 'text/xml; charset=utf-8'}
