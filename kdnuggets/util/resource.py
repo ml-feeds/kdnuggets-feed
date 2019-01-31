89c78a6502e8a9a497ed6c0fa49f2b52da004e1f
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-31 11:09:02 -0500

    Get utils from arxiv-ml-reviews repo for showing memory usage

---
+++ kdnuggets/util/resource.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from resource import getpagesize
+
+from kdnuggets.util.humanize import humanize_bytes
+
+
+_PAGESIZE = getpagesize()
+_PATH = Path('/proc/self/statm')
+
+
+def _resident_set_size() -> int:
+    """Return the current resident set size in bytes."""
+    # Ref: https://stackoverflow.com/a/53486808/
+    # statm columns are: size resident shared text lib data dt
+    statm = _PATH.read_text()
+    fields = statm.split()
+    return int(fields[1]) * _PAGESIZE
+
+
+def resident_set_size(subtract: int = 0) -> int:
+    return _resident_set_size() - subtract
+
+
+def humanized_rss(subtract: int = 0) -> str:
+    return humanize_bytes(resident_set_size(subtract))
